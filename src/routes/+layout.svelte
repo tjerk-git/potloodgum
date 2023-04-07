82a08625fe125c3ae1bda0e472476739e64d24ef
Tjerk Dijkstra <71773101+tjerk-git@users.noreply.github.com>
2023-04-07 17:49:05 +0200

    all

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+    import "../lib/style/global.css";
+</script>
+
+<main>
+    <slot />
+</main>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,24 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<section class="brand">
+    <h1>Potloodgum</h1>
+</section>
+
+<style>
+    .brand {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        height: 100vh;
+    }
+
+    .brand h1 {
+        font-size: 5rem;
+        color: #fff;
+        font-weight: 400;
+    }
+
+    @media screen and (max-width: 600px) {
+        .brand h1 {
+            font-size: 3rem;
+        }
+    }
+</style>
