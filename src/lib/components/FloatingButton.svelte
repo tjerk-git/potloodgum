d783755be579d45a26cb6c7a0450ac8ad186594e
Tjerk Dijkstra <tjerk.dijkstra@icloud.com>
2023-04-09 21:24:26 +0200

    fix

--- src/lib/components/FloatingButton.svelte
+++ src/lib/components/FloatingButton.svelte
@@ -54,47 +54,6 @@
 </a>
 
 <style>
-    .rainbow {
-        position: relative;
-        z-index: 0;
-        border-radius: 10px;
-        overflow: hidden;
-        padding: 2rem;
-    }
-
-    .rainbow::before {
-        content: "";
-        position: absolute;
-        z-index: -2;
-        left: -50%;
-        top: -50%;
-        width: 200%;
-        height: 200%;
-        background-color: var(--primary-color);
-        background-repeat: no-repeat;
-        background-size: 50% 50%, 50% 50%;
-        background-position: 0 0, 100% 0, 100% 100%, 0 100%;
-        background-image: linear-gradient(
-                var(--third-color),
-                var(--third-color)
-            ),
-            linear-gradient(var(--secondary-color), var(--secondary-color)),
-            linear-gradient(var(--primary-color), var(--primary-color)),
-            linear-gradient(var(--primary-color), var(--primary-color));
-        animation: rotate 10s linear infinite;
-    }
-
-    .rainbow::after {
-        content: "";
-        position: absolute;
-        z-index: -1;
-        left: 6px;
-        top: 6px;
-        width: calc(100% - 12px);
-        height: calc(100% - 12px);
-        background: white;
-        border-radius: 5px;
-    }
     .button {
         position: fixed;
         top: 20px;
@@ -107,8 +66,8 @@
         height: 100px;
     }
     .menu_container {
-        width: 100vw;
-        height: 100vh;
+        width: 100%;
+        height: 100%;
         background: var(--primary-color);
         transition: all 1s;
     }
