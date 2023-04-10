da1bdaf3d4a40a93a6aa89868d70abeb1f104351
Tjerk Dijkstra <tjerk.dijkstra@icloud.com>
2023-04-10 16:29:29 +0200

    dingen about enzo

---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,28 @@
+<script>
+    import Header from "$lib/components/Header.svelte";
+</script>
+
+<Header />
+
+<section>
+    <h1>Potloodgum</h1>
+    <summary>
+        Potloodgum is <a href="https://tjerk.art">Tjerk Dijkstra</a>
+
+        <ul>
+            <li>UX / UI</li>
+            <li>Frontend</li>
+            <li>Backend</li>
+            <li>Human Centered Design</li>
+        </ul>
+    </summary>
+</section>
+
+<style>
+    section {
+        display: flex;
+        background: white;
+        width: 100%;
+        padding: 20px;
+    }
+</style>
